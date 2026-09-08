4534b8f07f48d58762171dc3079e957a7cfba728
string2015 <string2015@yonsei.ac.kr>
2026-09-08 09:37:04 +0900

    [level 0] Title: 코드 처리하기, Time: 6.58 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ Python/프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,21 @@
+def solution(code):
+    mode = 0
+    ret = ""
+    for i in range(len(code)):
+        if mode == 0:
+            if code[i] != "1":
+                if i%2==0:
+                    ret += code[i]
+            else:
+                mode = 1
+
+        elif mode == 1:
+            if code[i] != "1":
+                if i%2 !=0:
+                    ret +=code[i]
+            else:
+                mode = 0
+                continue
+    if not ret:
+        return "EMPTY"
+    return ret
