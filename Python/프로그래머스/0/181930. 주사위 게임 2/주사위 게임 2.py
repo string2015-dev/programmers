26bfb0c745a082dbbc30bf859926cc48c6c342a3
string2015 <string2015@yonsei.ac.kr>
2026-09-08 16:11:44 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ Python/프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -0,0 +1,6 @@
+def solution(a, b, c):
+    if a == b == c:
+        return (a+b+c)*(a**2+b**2+c**2)*(a**3+b**3+c**3)
+    elif len({a,b,c}) == 3:
+        return a+b+c
+    else: return (a+b+c)*(a**2+b**2+c**2)
